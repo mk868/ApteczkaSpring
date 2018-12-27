a314e670a1f01e0ebc582c46598e6e5534639d39
mkucharczyk <maciej.kucharczyk@hotmail.com>
2018-12-27 13:58:28 +0100

    cleanup

--- Apteczka/Apteczka.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Apteczka.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Apteczka/Apteczka.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Apteczka.API.WebApiApplication" Language="C#" %>
